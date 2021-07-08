ac162be4a79853af165506036d7b27050133a582
Kenny Ha <77214322+altkennyh2l@users.noreply.github.com>
2021-07-08 17:17:53 +0900

    Added one thing, yes.

---
+++ JumpingPSController.cs
@@ -0,0 +1,20 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common;
+
+public class JumpingPSController : UdonSharpBehaviour
+{
+    public GameObject PSParent;
+    public override void InputJump(bool value, UdonInputEventArgs args)
+    {
+        GameObject SpawnedPSParent = VRCInstantiate(PSParent);
+        Vector3 SpawnedPSParentPos = Networking.LocalPlayer.GetPosition();
+        SpawnedPSParentPos.y = 0;
+        SpawnedPSParent.transform.position = SpawnedPSParentPos;
+        SpawnedPSParent.SetActive(true);
+        Destroy(SpawnedPSParent, 1);
+    }
+}
